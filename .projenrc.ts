7b54497be9199b927b67fb2d115a994aed5e7a2e
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-10-05 22:47:20 +0900

    inital commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,16 +2,29 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Kazuho CryerShinozuka',
   authorAddress: 'malaysia.cryer@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.130.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.5.0',
   name: 'cdk-private-s3-hosting',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/badmintoncryer/cdk-private-s3-hosting.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  keywords: ['aws', 'cdk', 'ec2', 'nodejs', 'aws-cdk', 'vscode'],
+  gitignore: ['*.js', '*.d.ts', '!test/.*.snapshot/**/*', '.tmp'],
+  deps: [],
+  description: 'CDK Construct for a private frontend hosting S3 bucket',
+  devDeps: [
+    '@aws-cdk/integ-runner@2.130.0-alpha.0',
+    '@aws-cdk/integ-tests-alpha@2.130.0-alpha.0',
+  ],
+  releaseToNpm: true,
+  packageName: 'cdk-private-s3-hosting',
+  publishToPypi: {
+    distName: 'cdk-private-s3-hosting',
+    module: 'cdk-private-s3-hosting',
+  },
 });
+
+project.projectBuild.testTask.exec(
+  'yarn tsc -p tsconfig.dev.json && yarn integ-runner',
+);
 project.synth();
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# replace this
+# CDK Private S3 Hosting Construct
+
+This is a CDK construct that creates a private S3 bucket and an Application Load Balancer (ALB) with a listener rule that forwards requests to the S3 bucket.
+
+You can use this construct for a enterprise use case where you want to host a static website in a private network.
+
+Original idea is from [this blog post](https://aws.amazon.com/jp/blogs/networking-and-content-delivery/hosting-internal-https-static-websites-with-alb-s3-and-privatelink/).
+
+## Architecture
+
+## Usage
+
